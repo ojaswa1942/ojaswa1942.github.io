bc8fef5fa9a4d629c9f95683d05d038073c0434b
Ojaswa Sharma <ojaswa1942@gmail.com>
2018-08-31 02:47:12 +0530

    Refactored

--- src/App.js
+++ src/App.js
@@ -3,26 +3,12 @@ import './App.css';
 import logo from './assets/pictures/name-white.png';
 import Welcome from './Components/Welcome/Welcome'
 import AboutMe from './Components/AboutMe/AboutMe'
-
-
-const goToAboutMe = () => {
-	const arrow = document.getElementById("goDown");
-	const aboutMeSection = document.getElementById("aboutMe");
-	const goToAbout = () => {
-		aboutMeSection.scrollIntoView({
-			behavior: "smooth",
-			block: "start"
-		})
-	}
-	arrow.addEventListener("click", goToAbout);
-}
+import {goToAboutMe} from './assets/navFunc'
 
 class App extends Component {
-
 	componentDidMount(){
 		goToAboutMe();
 	}
-
 	render(){
 		return(
 		  <div className="App">
