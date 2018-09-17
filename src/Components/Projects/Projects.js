fd0a37a9e0e17f4095c5d103fa748097b4921385
Ojaswa Sharma <ojaswa1942@gmail.com>
2018-09-17 22:17:14 +0530

    Adding project cards

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Projects.css'
+import p1 from '../../assets/projects/p1.png'
+import p2 from '../../assets/projects/p2.png'
 
 const Projects = () =>{
 	return(
@@ -11,8 +13,8 @@ const Projects = () =>{
 
 					<div className="" id='projectsSection-cards'>
 						
-						<div className='card bg-dark-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-							<img  alt='Project' src ={`https://robohash.org/123?200x200`} className='projectPic'/>
+						<div className='card bg-dark-green dib br3 pa3 ma3 grow bw2 shadow-5'>
+							<img  alt='Project' src ={p1} className='projectPic'/>
 							<div>
 								<h2>
 									<a href='https://face-recogniti.herokuapp.com/' className='link underline white dim'>
@@ -25,16 +27,16 @@ const Projects = () =>{
 							</div>
 						</div>
 
-						<div className='card bg-dark-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-							<img  alt='Project' src ={`https://robohash.org/123?200x200`} className='projectPic'/>
+						<div className='card bg-dark-green dib br3 pa3 ma3 grow bw2 shadow-5'>
+							<img  alt='Project' src ={p2} className='projectPic'/>
 							<div>
 								<h2>
-									<a href='https://face-recogniti.herokuapp.com/' className='link underline white dim'>
-										Face Recognition Web App
+									<a href='https://ojaswa1942.github.io/robofriends' className='link underline white dim'>
+										Robo Friends Web App
 									</a>
 								</h2>
 								<p>
-									A full-stack working web application using Clarifai Web API for face detection in an image, with website designed using REACTjs, server using NODE with EXPRESS.js and PostgreSQL for database. The server, the database and the front-end are all deployed on Heroku.
+									An entry-level web application made using REACT, which views contact cards of robot friends and a search bar which dynamically searches with input.	The application later integrated with REDUX can be found <a href='https://github.com/ojaswa1942/robofriends-redux' className='link underline white dim'>here</a>. 							
 								</p>
 							</div>
 						</div>
